911f835a2ab9391dc83413ef933a669f3f1a75d4
wxzxmqx <yatsunenko.serhii@hneu.net>
2025-05-26 20:53:49 +0200

    day 4 :)

---
+++ Part 1 - Fundamentals/Part 1.13 - Boolean Logic/boolean_logic.js
@@ -0,0 +1,15 @@
+// Boolean logic: the and, or & not operators
+
+// AND operator              AND |true  | false |
+//                         ------|------|-------|
+// true when ALL are true  true  |true  | false |
+//                         ------|------|-------|
+//                         false |false | false |
+
+// OR operator              0R   | true | false |
+//                         ------|------|-------|
+// true when one is true   true  | true | true  |
+//                         ------|------|-------|
+//                         false | true | false |
+
+// NOT operator !
