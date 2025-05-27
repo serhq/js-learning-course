9db31bf748745f3eaf08b686501a36e50616b3f4
serhq <yatsunenko.serhii@hneu.net>
2025-05-27 21:23:40 +0200

    day 5 ;)

---
+++ Part 2 - Fundamentals/2.0 - Strict Mode/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// ALWAYS TURN IT ON!
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive 🎊');
+
+// const interface = 'Audio';
+// const private = 534;
