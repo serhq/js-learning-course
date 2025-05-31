9e15b79a5faad22ae1c29d54fa0b00732177299b
serhq <yatsunenko.serhii@hneu.net>
2025-05-31 18:12:05 +0200

    challenge completed

--- Part 2 - Fundamentals/Challenges/1.2 - Challenge/script.js
+++ Part 2 - Fundamentals/Challenges/1.2 - Challenge/script.js
@@ -1,13 +1,7 @@
-const calcTip = function(bills) {
-    if (bills >= 50 && bills <= 300) {
-        return bills * 0.15;
-    } else {
-        return bills * 0.20;
-    }
-}
+const calcTip = bills => (bills >= 50 && bills <= 300) ? bills * 0.15 : bills * 0.20;
 
-let bills = [125, 555, 44];
-let tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
-let totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[bills.length - 1])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 
-console.log(bills, tips, totals);
+console.log(totals);
