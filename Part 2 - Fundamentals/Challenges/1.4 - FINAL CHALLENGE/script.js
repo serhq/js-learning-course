58a60a00549db8064894f18c3123886f78691283
serhq <yatsunenko.serhii@hneu.net>
2025-06-02 22:05:25 +0200

    some adjustments

--- Part 2 - Fundamentals/Challenges/1.4 - FINAL CHALLENGE/script.js
+++ Part 2 - Fundamentals/Challenges/1.4 - FINAL CHALLENGE/script.js
@@ -14,3 +14,13 @@ for (let i = 0; i <= bills.length - 1; i++) {
 }
 
 console.log(totals);
+
+const calcAverage = function(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+
+console.log(calcAverage(totals).toFixed(1));
