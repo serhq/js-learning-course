3644f56852cccddff9853147b701b8f31c64b5b1
serhq <yatsunenko.serhii@hneu.net>
2025-06-16 22:10:39 +0200

    moved to the folder

--- Part 4 - DOM and Events/4.1 - Project/script.js
+++ Part 4 - DOM and Events/4.1 - Project/script.js
@@ -7,6 +7,7 @@ const btnsShowModal = document.querySelectorAll('.show-modal');
 
 console.log(btnsShowModal);
 
+
 const openModal = function () {
     modal.classList.remove('hidden'); // classList - change the class (add/remove)
     overlay.classList.remove('hidden');
@@ -16,6 +17,10 @@ const closeModal = function () {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
 };
+
+document.addEventListener('keydown', function (e) { // get the information about the key that was pressed where -> (e) means the parameter of the pressed key
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) closeModal();
+}); // press the key 
 
 for (let i = 0; i < btnsShowModal.length; i++) {
     btnsShowModal[i].addEventListener('click', openModal);
